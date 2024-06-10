fb29e95b8002d695a2c9ac2ae36436acba5d07b5
AnthonyRivera <AnthonyRivera@DESKTOP-Q3J64FT>
2024-06-10 15:29:20 -0400

    Se hizo tarea de lecture 3 haciendo ejercicio de Arreglo y Funciones.

---
+++ Lecture3-Tarea/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] ages = { 20, 28, 29, 69, 7 };
+string[] names = { "Pepe", "Anthony", "Pablo", "Juana", "Leticia" };
+
+for(int i = 0; i < ages.Length; i++)
+{
+    Console.WriteLine($"Nombre: {names[i]}, Edad: {ages[i]}");
+}
+
+
+for(int i = 0;i < ages.Length; i++)
+{
+    Saludo(ages[i], names[i]);
+}
+
+static void Saludo(int age, string name)
+{
+    Console.WriteLine($"Say Hello World! {name}. Is your birthday number {age}");
+}
+
+Console.ReadKey();
