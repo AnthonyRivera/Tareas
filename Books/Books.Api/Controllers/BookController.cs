55cde15a1d15d74d78b3008081410e595f197e78
AnthonyRivera <AnthonyRivera@DESKTOP-Q3J64FT>
2024-07-10 17:26:55 -0400

    Added CRUD functions

--- Books/Books.Api/Controllers/BookController.cs
+++ Books/Books.Api/Controllers/BookController.cs
@@ -1,7 +1,9 @@
 ﻿using Books.Domain.Models;
 using Books.Persistence.Context;
 using Books.Web.Interfaces;
+using Books.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Books.Api.Controllers
 {
@@ -9,18 +11,100 @@ namespace Books.Api.Controllers
     [Route("[controller]")]
     public class BookController:ControllerBase
     {
-        private readonly IBookRepository _context;
+        private readonly IBookRepository _bookRepository;
+        private readonly ApplicationContext _context;
+        private readonly BookAdoRepository _bookAdoRepository;
 
-        public BookController(IBookRepository context)
+
+        public BookController(IBookRepository bookRepository, ApplicationContext context, BookAdoRepository bookAdoRepository)
         {
-            _context = context;
+            _bookRepository = bookRepository;
+            _context= context;
+            _bookAdoRepository = bookAdoRepository;
+
+
         }
 
         [HttpGet(Name = "GetBooks")]
         public IEnumerable<Book> Get()
         {
-            var booksFromDb = _context.GetAll();
+            var booksFromDb = _bookRepository.GetAll();
             return booksFromDb;
         }
+
+        [HttpGet("{id}", Name = "GetBook")]
+        public ActionResult<Book> Get(int id)
+        {
+            var booksFromDb = _context.Book.FirstOrDefault(p => p.BookId == id);
+            if (booksFromDb == null)
+            {
+                return NotFound("Book not found");
+            }
+            return Ok(booksFromDb);
+        }
+
+        [HttpPost(Name = "CreateBook")]
+        public async Task<IActionResult> Create([FromBody] Book model)
+        {
+            if (model == null)
+            {
+                return BadRequest("Book data is null");
+            }
+
+            if (ModelState.IsValid)
+            {
+                
+                _context.Book.Add(model);
+                await _context.SaveChangesAsync();
+                return CreatedAtRoute("GetBook", new { id = model.BookId }, model);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}", Name = "UpdateBook")]
+        public async Task<IActionResult> Update(int id, [FromBody] Book model)
+        {
+            if (model == null || id != model.BookId)
+            {
+                return BadRequest("Book data is invalid");
+            }
+
+            var bookFromDb = await _context.Book.FindAsync(id);
+            if (bookFromDb == null)
+            {
+                return NotFound("Book not found");
+            }
+
+            if (ModelState.IsValid)
+            {
+                bookFromDb.ReleaseYear = model.ReleaseYear;
+                bookFromDb.Author = model.Author;
+                bookFromDb.BookName = model.BookName;
+
+                _context.Book.Update(bookFromDb);
+                await _context.SaveChangesAsync();
+                return Ok(bookFromDb);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}", Name = "DeleteContact")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _context.Book.FindAsync(id);
+            if (bookFromDb == null)
+            {
+                return NotFound("Contact not found");
+            }
+
+            _context.Book.Remove(bookFromDb);
+            await _context.SaveChangesAsync();
+            return Ok("Contact Deleted");
+        }
     }
+
+
+
 }
--- Books/Books.Web/Controllers/BookController.cs
+++ Books/Books.Web/Controllers/BookController.cs
@@ -1,7 +1,10 @@
 ﻿using Books.Persistence.Context;
 using Books.Web.Interfaces;
 using Books.Web.Repositories;
+using Books.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
+using Books.Web.Models;
+using System.Diagnostics;
 namespace Lecture19_Tarea.Controllers
 {
     public class BookController:Controller
@@ -21,6 +24,65 @@ namespace Lecture19_Tarea.Controllers
             //var cotacts = _context.Contacts.ToList();
             var books = _bookRepository.GetAll();
             return View(books);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Book model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            _bookRepository.Add(model);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var contactFromDb = _bookRepository.GetById(id);
+            if (contactFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(contactFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Book model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            _bookRepository.Update(model);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var contactFromDb = _bookRepository.GetById(id);
+            if (contactFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(contactFromDb);
+        }
+        [HttpPost]
+        public IActionResult Delete(Book model)
+        {
+            _bookRepository.Delete(model.BookId);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
